1a6db31835538dd51db63f3faeabeaf3d7c62414
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2022-06-28 22:59:34 +0200

    Implemented Vuetify plugin and tested component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
+  build: {
+    transpile: ['vuetify'],
+  },
 })
