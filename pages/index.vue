5edabd81c1a69e3bde638f87332a89e9a03c1a37
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2022-06-28 23:55:18 +0200

    feat: now App.vue uses pages and also created a new login page

---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="d-flex justify-center">
+    <v-app>
+      <v-main class="d-flex justify-center align-center">
+        <v-col
+          cols="10"
+          lg="4"
+          style="margin-left: 16%; padding-left: 10px"
+          class="text-left"
+        >
+          <v-form>
+            <v-card class="text-center" width="550">
+              <client-only>
+                <v-card-title
+                  primary-title
+                  class="justify-center font-weight-bold"
+                >
+                  Login Form
+                </v-card-title>
+                <v-card-text>
+                  <v-text-field
+                    label="Username"
+                    placeholder="Username"
+                    variant="outlined"
+                    prepend-inner-icon="mdi-account"
+                    autocomplete="on"
+                  />
+                  <v-text-field
+                    type="password"
+                    label="Password"
+                    placeholder="Password"
+                    variant="outlined"
+                    prepend-inner-icon="mdi-key"
+                    append-inner-icon="mdi-eye"
+                    autocomplete="on"
+                  />
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn
+                    variant="outlined"
+                    flat
+                    color="secondary"
+                    class="ml-8 mb-3"
+                  >
+                    Register
+                  </v-btn>
+                  <v-spacer />
+                  <v-btn variant="outlined" flat color="info" class="mr-8 mb-3">
+                    Login
+                  </v-btn>
+                </v-card-actions>
+              </client-only>
+            </v-card>
+          </v-form>
+        </v-col>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style>
+div[data-app='true'] {
+  background: url('/assets/bg1.svg') no-repeat bottom right fixed !important;
+}
+</style>
